1725a8aeea2dede0c6899fd091d755e47ad706ce
zolinek89 <zolinek89@gmail.com>
2016-09-26 22:20:23 +0200

    Dodanie KursyContext

---
+++ SklepInternetowy/DAL/KursyContext.cs
@@ -0,0 +1,21 @@
+﻿using SklepInternetowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.DAL
+{
+    public class KursyContext: DbContext
+    {
+        public KursyContext() : base("KursyContext")
+        {
+
+        }
+        public DbSet<Kurs> Kursy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+    }
+}
