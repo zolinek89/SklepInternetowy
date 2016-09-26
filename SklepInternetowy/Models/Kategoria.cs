eaebcd939cff057f0783f6e382b07ed8bbcb1f5e
zolinek89 <zolinek89@gmail.com>
2016-09-26 21:26:00 +0200

    Stworzenie projektu i dodanie klas modelu

---
+++ SklepInternetowy/Models/Kategoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace SklepInternetowy.Models 
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategorii { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+
+        public virtual ICollection<Kurs> Kursy { get; set; } 
+    }
+}
---
+++ SklepInternetowy/Models/Zamowienie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.Models
+{
+    public class Zamowienie
+    {
+        public int ZamowienieId { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Ulica { get; set; }
+        public string Miasto { get; set; }
+        public string Telefon { get; set; }
+        public string KodPocztowy { get; set; }
+        public string Email { get; set; }
+        public string Komentarz { get; set; }
+        public DateTime DataDodania { get; set; }
+        public decimal WartoscZamowienia { get; set; }
+        public StanZamowienia StanZamowienia { get; set; }
+
+        List<PozycjaZamowienia> PozycjaZamowienia { get; set; }
+
+    }
+    public enum StanZamowienia
+    {
+        Nowe,
+        Zrealizowane
+    }
+}
